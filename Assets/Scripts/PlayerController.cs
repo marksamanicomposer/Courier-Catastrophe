87ddb868f77a10ad658ae56589028e06388c1bf3
Kira Pavlovena <zpavrd@gmail.com>
2023-07-08 14:09:56 -0400

    renamed the input system asset, flapping done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,61 +7,67 @@ public class PlayerController : MonoBehaviour
 {
     //movment fields
     private Rigidbody rb;
-    [SerializeField] private float maxPitch = 180f; //max angle (in degrees) the model can pitch in flight
-    [SerializeField] private float maxRoll = 180f; //max angle (in degrees) the model can roll in flight
-    [SerializeField] private float maxYaw = 180f; //max angle (in degrees) the model can yaw in flight (not currently used)
-    [SerializeField] private float rollInterval = .5f; //how much the player will roll while holding down the corresponding key
-    [SerializeField] private float pitchInterval = .5f; //how much the player will pitch up/down while holding down the corresponding key
-    [SerializeField] private float forwardForce;
-    [SerializeField] private float gravityForce = 9.8f;
-    [SerializeField] private float flapDelay; //time in seconds between flaps
-    private bool isLanded = true;
-    private Vector3 playerHeading = Vector3.zero;
+    [SerializeField] [Tooltip("max angle (in degrees) the model can pitch in flight")] private float maxPitch = 90f; 
+    [SerializeField] [Tooltip("max angle (in degrees) the model can roll in flight")] private float maxRoll = 90f;
+    [SerializeField] [Tooltip("max angle (in degrees) the model can yaw in flight (not currently used)")] private float maxYaw = 90f; 
+    [SerializeField] [Tooltip("how much the player will roll while holding down the corresponding key")] private float rollInterval = .5f; 
+    [SerializeField] [Tooltip("how much the player will pitch up/down while holding down the corresponding key")] private float pitchInterval = .5f;
+    [SerializeField] private float flapForce = 20f;
+    [SerializeField] private float takeoffForce = 1000f;
+    [SerializeField] [Tooltip("time in seconds between flaps")] private float flapDelay = 3f;
+    private float flapTimer;
+    private bool isLanded = false;
 
     //input fields
-    private PlayerActions playerActions;
+    private PlayerActionController playerAction;
     private InputAction roll, pitch;
 
     private void Awake() {
         rb = this.GetComponent<Rigidbody>();
-        playerActions = new PlayerActions();
+        playerAction = new PlayerActionController();
     }
 
     private void OnEnable() {
-        playerActions.Player.Coo.started += DoCoo;
-        playerActions.Player.Flap.started += DoFlap;
+        playerAction.Player.Coo.started += DoCoo;
+        playerAction.Player.Flap.started += DoFlap;
 
-        roll = playerActions.Player.Roll;
-        pitch = playerActions.Player.Pitch;
+        roll = playerAction.Player.Roll;
+        pitch = playerAction.Player.Pitch;
 
-        playerActions.Player.Enable();
+        playerAction.Player.Enable();
     }
 
     private void OnDisable() {
-        playerActions.Player.Coo.started -= DoCoo;
-        playerActions.Player.Flap.started -= DoFlap;
+        playerAction.Player.Coo.started -= DoCoo;
+        playerAction.Player.Flap.started -= DoFlap;
 
-        playerActions.Player.Disable();
+        playerAction.Player.Disable();
     }
 
     private void FixedUpdate() {
-        //TODO: cast a ray from front of bird (tip of beak maybe?) to create a heading, have the bird along that heading
-        //do the ray casting when updating the bird's position possibly for better performance?
+        //TODO: pitch and roll code
     }
 
 
     //flaps the player's wings or allows them to take off
     private void DoFlap(InputAction.CallbackContext obj) {
+
         if(isLanded) {
             Takeoff();
-        } //TODO: else: have the flap animation play, raise the height of the player slightly relative to the orientation of the model, reset the flap delay timer
+        } else {
+            rb.AddForce(transform.up * flapForce, ForceMode.Impulse);
+        }
+
+        flapTimer = flapDelay;
+
     }
 
     private void DoCoo(InputAction.CallbackContext obj) {
-        throw new NotImplementedException();
+        Debug.Log("Coo!");
     }
 
     private void Takeoff() {
+        rb.AddForce((new Vector3(0,1,1) * takeoffForce), ForceMode.Impulse);
         isLanded = false;
     }
 
