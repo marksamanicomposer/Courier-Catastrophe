da9cdff885a180fdd5374103f9449d5f92703e3e
jclibs <78166933+jclibs@users.noreply.github.com>
2023-07-26 21:42:21 -0400

    alternateControls

---
+++ Assets/Scripts/PigeonCam.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigeonCam : MonoBehaviour
+{
+
+    public GameObject transformTarget, pigeon;
+    public float moveSpeed, distance, maxDistance;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       // moveSpeed = .17f; //these values feel pretty good
+       // maxDistance = 15;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transformTarget.transform.position, moveSpeed); //camera follows your position
+
+        transform.LookAt(pigeon.transform); //camera looks at player
+
+        distance = Vector3.Distance(transform.position, transformTarget.transform.position); //calculates distance between camera and player
+
+
+        if (distance >= maxDistance)
+        {
+            moveSpeed = .3f; //camera speeds up if you get too far away
+        }
+        else moveSpeed = .17f;
+    }
+}
---
+++ Assets/Scripts/PigeonControls.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigeonControls : MonoBehaviour
+{
+    [SerializeField] private float speed, turnSpeed;
+    public float pitch, yaw;
+    Animator anim;
+    public float multiplier;
+    public bool isFlying = true;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.Play("flight");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                   
+            transform.Translate(0, Mathf.Sin(Time.time * 2 - 2) * Time.deltaTime * multiplier, 0, Space.World); //hover up and down a bit
+
+            pitch += turnSpeed * Input.GetAxis("Vertical");//*-1;   //turn direction up/down
+            yaw += turnSpeed * Input.GetAxis("Horizontal");        //turn direction left/right
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0f); // tilt prevention
+
+            transform.Translate(Vector3.back * Time.deltaTime * speed); //forward motion
+
+           
+    }
+
+
+   
+    
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Terrain"))
+        {
+            anim.Play("walk");
+            Debug.Log("touchingTerrain");
+            isFlying = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Terrain"))
+        {
+            anim.Play("flight");
+            isFlying = true;
+        }
+    }
+}
