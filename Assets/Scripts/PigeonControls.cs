48bbbbe3c121ea3ed811cbc07dcb1f94d650f3ca
marksamanicomposer <98137923+marksamanicomposer@users.noreply.github.com>
2023-07-27 15:48:56 -0400

    Edited SFX volume in gameplay
    - And set pigeon's speed/turn speed variables to private without serializing

--- Assets/Scripts/PigeonControls.cs
+++ Assets/Scripts/PigeonControls.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PigeonControls : MonoBehaviour
 {
-    [SerializeField] private float speed, turnSpeed;
+    private float speed = 10;
+    private float turnSpeed = 1.6f;
     public float pitch, yaw;
     Animator anim;
     public float multiplier;
